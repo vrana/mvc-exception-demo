06289a7c0272292ba0966d00c803eff0a8f128f3
Jakub Vrana <jakub@vrana.cz>
2010-08-10 15:30:48 +0200

    Whitespace

--- presenters/ArticlePresenter.php
+++ presenters/ArticlePresenter.php
@@ -3,9 +3,9 @@ require_once APP_DIR . '/models/Article.php';
 require_once dirname(__FILE__) . '/BasePresenter.php';
 
 class ArticlePresenter extends BasePresenter {
-    function renderDetail($id) {
-        // tady bude načtení hlavního obsahu stránky - detailu článku
-        // kromě toho načteme i výběr článků pro postranní menu
-        $this->template->bestArticles = Article::findBests();
-    }
+	function renderDetail($id) {
+		// tady bude načtení hlavního obsahu stránky - detailu článku
+		// kromě toho načteme i výběr článků pro postranní menu
+		$this->template->bestArticles = Article::findBests();
+	}
 }
